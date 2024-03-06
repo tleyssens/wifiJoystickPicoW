eb803dffe2d2d4ce1b8c36140773f410f66e9ceb
Tom Leyssens <tleyssens@users.noreply.github.com>
2024-03-06 18:12:59 +0100

    cleanup

--- motor.py
+++ motor.py
@@ -9,15 +9,19 @@ class Motor():
         self.PWMpin = PWM(Pin(PWMpin, Pin.OUT), freq=50, duty_u16=0)
         self.DIRpin = Pin(DIRpin, Pin.OUT)
         self.previousMillis = 0
+
     @property
     def PWM(self):
         return self.PWMpin.duty_u16()
+
     @PWM.setter
     def PWM(self, value):
         self.PWMpin.duty_u16(value)
+
     @property
     def DIR(self):
         return self.DIRpin.value()
+
     @DIR.setter
     def DIR(self, value):
         self.DIRpin.value(value)
